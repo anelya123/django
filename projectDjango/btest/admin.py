c67cca97c630f145e6aff2c6b03478364429dcb8
anelya123 <ma.anelya@gmail.com>
2022-01-20 14:12:57 +0300

    add

---
+++ projectDjango/btest/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Bb
+from .models import Rubric
+
+class BbAdmin(admin.ModelAdmin) :
+	list_display = ('title', 'content', 'price', 'published', 'rubric')
+	list_display_links = ('title', 'content')
+	search_fields = ('titile', 'content', 'price', 'published')
+
+admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric)
+
+# Register your models here.
